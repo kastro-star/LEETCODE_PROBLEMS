b3cbfdff6264b80735107c37e31a5dc18beb68db
kastro-star <kastrouchiha@gmail.com>
2026-02-12 23:39:20 +0530

    again

---
+++ ZOHO_QUESTIONS/pattern/Right-Aligned Sequential Number Pattern Without Array.java
@@ -0,0 +1,9 @@
+
+
+public class Right-Aligned Sequential Number Pattern Without Array {
+    //               1
+    //            6  2
+    //       10   7  3
+    //    13   11  8 4
+    // 15   14  12 9  5             
+}
